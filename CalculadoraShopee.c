5a3bb2cff9417a4e6e20bdb73b5383163b954d75
Thiago Dev <devthiagovieira@gmail.com>
2024-01-13 16:37:12 -0300

    Atualização de valores e opções

--- CalculadoraShopee.c
+++ CalculadoraShopee.c
@@ -6,16 +6,18 @@ int main(){
 	setlocale(LC_ALL, "Portuguese");
 	float custoFolha, quantidadeFolhas, precoVenda, precoVendaShopee, porcentagemShopee;
 	int num, i = 1;
-	
-	printf("=============== Calculadora de preços TG Impressos ===============\n");
+	printf("===================================================================\n");	
+	printf("===================== CALCULADORA TG IMPRESSOS ====================\n\n");
+	printf(" Desenvolvedor: Thiago Vieira                          Versão 2.0\n");
 
 	do{
-		printf("==================================================================\n");	
-	    printf(" \nDigite o número de acordo com a opção desejada: \n");
-	    printf(" (1) Cartão com corte na máquina;\n");
-	    printf(" (2) Cartão com corte manual;\n");
-	    printf(" (3) Folhetos apenas frente A6;\n");
-	    printf(" (4) Adesivos;\n");
+		printf("===================================================================\n\n");	
+	    printf(" Digite o número de acordo com a opção desejada:\n\n");
+	    printf(" (1) Adesivos;\n");
+	    printf(" (2) Etiquetas;\n");
+	    printf(" (3) Cartão de Visitas 54x90mm;\n");
+	    printf(" (4) Cartão com Corte Personalizado;\n");
+	    printf(" (5) Folhetos Apenas Frente A6;\n");
 	    scanf("%d",&num);
 	    fflush(stdin);
 	    
@@ -27,57 +29,54 @@ int main(){
 	    fflush(stdin);
 	    
 	    switch(num){
-	        case 1 : 
-	            if (quantidadeFolhas < 15) {
-	                precoVenda = ((custoFolha * quantidadeFolhas) + 1) * 3 + 3;
-	            } else {
-	               precoVenda = ((custoFolha * quantidadeFolhas) + 2) * 3 + 3;
-	            }
-	        break;
-	        
-	        case 2 :
-	            if (custoFolha < 0.30) {
-	                precoVenda = ((custoFolha * quantidadeFolhas) + 1.5) * 3 + 3;
-	            } else if (custoFolha >=0.30 && custoFolha < 0.70) {
-	                precoVenda = ((custoFolha * quantidadeFolhas) + 2.5) * 3 + 3;
-	            } else {
-	                precoVenda = ((custoFolha * quantidadeFolhas) + 3.5) * 3 + 3;
-	            }
+	        case 1 || 2 : 
+	        	precoVenda = ((custoFolha * quantidadeFolhas) + 3) * 3 + 3;
 	        break;
 	        
 	        case 3 : 
-	            precoVenda = ((custoFolha * quantidadeFolhas) + 1) * 2.7 + 3;
+	            if(quantidadeFolhas < 12){
+	                precoVenda = ((custoFolha * quantidadeFolhas) + 1.5) * 3 + 3;
+	        	}else{
+	            	precoVenda = ((custoFolha * quantidadeFolhas) + 2) * 3 + 3;
+	        	}
 	        break;
 	        
 	        case 4 :
-	        	precoVenda = ((custoFolha * quantidadeFolhas) + 2.5) * 3 + 3;
+	        	if(custoFolha < 0.40){
+	                precoVenda = ((custoFolha * quantidadeFolhas) + 1.5) * 3 + 3;
+	            }else if(custoFolha >=0.40 && custoFolha < 0.70){
+	            	precoVenda = ((custoFolha * quantidadeFolhas) + 3) * 3 + 3;
+	            }else{
+	        		precoVenda = ((custoFolha * quantidadeFolhas) + 4) * 3 + 3;
+	        	}
 	        break;
 	        
+	        case 5 :
+	        	precoVenda = ((custoFolha * quantidadeFolhas) + 2.5) * 3 + 3;
+	        break;
+
 	        default:
 	            printf(" Número inválido!\n");
 	        break;
 	    }
-	    
 		porcentagemShopee = 25 * precoVenda / 100;
 		precoVendaShopee = precoVenda + porcentagemShopee;
 		    
-	    if (quantidadeFolhas >= 17 && custoFolha > 0.10){
-		    precoVenda += 4;
+	    if(quantidadeFolhas >= 17 && custoFolha > 0.10){
+		    precoVenda += 5;
 			precoVendaShopee += 5;
 		}
-		
-		printf("\n Preço de venda por fora ============ %.2f\n", precoVenda);
-		printf(" Preço de venda pela Shopee: ======== %.2f\n", precoVendaShopee);
+		printf(" Preço de venda por fora ================= %.2f\n", precoVenda);
+		printf(" Preço de venda pela Shopee: ============= %.2f\n", precoVendaShopee);
 		printf("\n");
-		printf("==================================================================\n");	
-		printf("\nFazer um novo calculo?\n");
+		printf("==================================================================\n\n");	
+		printf(" Deseja fazer um novo calculo?\n");
 		printf(" Digite o número correspondente a resposta!\n");
 		printf(" (1) SIM\n");
 		printf(" (2) NAO\n");
 		scanf("%d",&i);
 		fflush(stdin);
-		
-	}while (i==1);
+	} while (i==1);
 
 	system("pause");
     return 0;
